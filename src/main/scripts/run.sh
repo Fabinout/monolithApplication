bf0e0e52612ed5538e70e7e6e851797aef380d2a
Fabien Lamarque <flamarque@octo.com>
2018-04-14 11:50:00 +0200

    updated script

--- src/main/scripts/run.sh
+++ src/main/scripts/run.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
+# install ansible for the rest of your life
+brew install ansible
+
+#install your tools
+ansible-playbook -i "localhost," -c local src/main/ansible/playbook.yml
+
+# build and deploy your application
  ./mvnw verify -Pprod dockerfile:build
 docker-compose -f src/main/docker/app.yml up -d
+
+# deploy your ci-cd tool
 docker-compose -f src/main/docker/jenkins.yml up -d
